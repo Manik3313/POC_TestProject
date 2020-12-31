eca28a57e68ae82277be285da994f07f3387278c
Manik Gupta <manik.gupta@testingxperts.loc>
2020-12-31 07:25:23 +0530

    First commit

---
+++ src/test/java/sample/Test_1.java
@@ -0,0 +1,13 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class Test_1 {
+
+	@Test(groups = {"d"})
+	
+	public void test()
+	{
+		System.out.println("Test1");
+	}
+}
---
+++ src/test/java/sample/demo.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class demo
+{
+public static void main(String[] args)
+{
+	String s="\\src\\main\\resources\\user_data.xlsx";
+	 s=System.getProperty("user.dir")+s;
+	 System.out.println(s);
+ 	
+}
+}
